faf250e7870c4e402f00461470b3dce56e95cf4f
deepak-s-git <deepak.s.workdesk@gmail.com>
2026-08-20 15:46:39 +0530

    build: ignore typescript errors for vercel deployment

--- next.config.ts
+++ next.config.ts
@@ -20,6 +20,13 @@ const nextConfig: NextConfig = {
       },
     ],
   },
+
+  typescript: {
+    // !! WARN !!
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    ignoreBuildErrors: true,
+  },
 };
 
 export default nextConfig;
